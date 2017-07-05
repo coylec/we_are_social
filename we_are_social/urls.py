44cb48d468adb8c94be3ca9e2f7e43d6351e6648
Conrad Coyle <coylec@users.noreply.github.com>
2017-07-05 17:24:17 +0100

    Final forum file updates

--- we_are_social/urls.py
+++ we_are_social/urls.py
@@ -22,6 +22,7 @@ from paypal.standard.ipn import urls as paypal_urls
 from paypal_store import views as paypal_views
 from products import views as product_views
 from magazines import views as magazine_views
+from threads import views as forum_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -43,6 +44,15 @@ urlpatterns = [
 
     # Blog
     url(r'^blog/', include('reusable_blog.urls')),
+
+    # Forum
+    url(r'^forum/$', forum_views.forum),
+    url(r'^threads/(?P<subject_id>\d+)/$', forum_views.threads, name='threads'),
+    url(r'^new_thread/(?P<subject>\d+)/$', forum_views.new_thread, name='new_thread'),
+    url(r'^thread/(?P<thread_id>\d+)/$', forum_views.thread, name='thread'),
+    url(r'^post/new/(?P<thread_id>\d+)/$', forum_views.new_post, name='new_post'),
+    url(r'^post/edit/(?P<thread_id>\d+)/(?P<post_id>\d+)/$',forum_views.edit_post, name='edit_post'),
+    url(r'^post/delete/(?P<thread_id>\d+)/(?P<post_id>\d+)/$', forum_views.delete_post, name='delete_post'),
 ]
 
 if settings.DEBUG:
