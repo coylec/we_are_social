fef73075251bd0b25099fd211a134873153a9b57
Conrad Coyle <coylec@users.noreply.github.com>
2017-07-10 16:07:25 +0100

    Setup project for deploy

---
+++ settings/dev.py
@@ -0,0 +1,22 @@
+from base import *
+
+
+DEBUG = True
+
+INSTALLED_APPS.append('debug_toolbar')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Stripe environment variables
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_ch3eEaRHvrtWq2jb28V5PRfX')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_nfZf9yc1njMtRqBdVndurV0m')
+
+# PayPal environment variables
+SITE_URL = 'http://127.0.0.1:8000'
+PAYPAL_NOTIFY_URL = 'http://98de0985.ngrok.io/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'conradcoyle-facilitator@gmail.com'
--- threads/views.py
+++ threads/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from threads.models import Subject, Thread, Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
