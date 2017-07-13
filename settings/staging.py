faac2f4b293d3d8b42be7917ad4d37ff9229dd7d
Conrad Coyle <coylec@users.noreply.github.com>
2017-07-13 13:28:09 +0100

    Changed cleardb_url back to original version

--- settings/staging.py
+++ settings/staging.py
@@ -12,7 +12,7 @@ DATABASES = {
 }
 
 
-DATABASES['default'] = dj_database_url.config("mysql://beee9234b298d5:c59a3457@eu-cdbr-west-01.cleardb.com/heroku_523762a7b6c13e9?")
+DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
 
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_ch3eEaRHvrtWq2jb28V5PRfX')
